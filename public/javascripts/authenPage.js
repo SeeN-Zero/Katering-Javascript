763cfe160f24acdee9a00bf93c7449693a79b6c5
Senna Annaba Ahmad <71185988+seen-zero@users.noreply.github.com>
2022-07-04 19:39:25 +0700

    Number Configuration

--- public/javascripts/authenPage.js
+++ public/javascripts/authenPage.js
@@ -1,20 +1,19 @@
 window.onload = function () {
-    $('.toast').toast('show');
-    const signUpButton = document.getElementById('signUp');
-    const signInButton = document.getElementById('signIn');
-    const container = document.getElementById('container');
+  $('.toast').toast('show')
+  const signUpButton = document.getElementById('signUp')
+  const signInButton = document.getElementById('signIn')
+  const container = document.getElementById('container')
 
-    signUpButton.addEventListener('click', () => {
-        container.classList.add("right-panel-active");
-        document.getElementById("usernamelog").value = "";
-        document.getElementById("passwrodlog").value = "";
-    });
+  signUpButton.addEventListener('click', () => {
+    container.classList.add('right-panel-active')
+    document.getElementById('usernamelog').value = ''
+    document.getElementById('passwrodlog').value = ''
+  })
 
-    signInButton.addEventListener('click', () => {
-        container.classList.remove("right-panel-active");
-        document.getElementById("namereg").value = "";
-        document.getElementById("usernamereg").value = "";
-        document.getElementById("passwordreg").value = "";
-    });
-};
-
+  signInButton.addEventListener('click', () => {
+    container.classList.remove('right-panel-active')
+    document.getElementById('namereg').value = ''
+    document.getElementById('usernamereg').value = ''
+    document.getElementById('passwordreg').value = ''
+  })
+}
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,64 +1,64 @@
-window.addEventListener("DOMContentLoaded", (event) => {
-    // Navbar shrink function
-    const navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector("#mainNav");
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove("navbar-shrink");
-        } else {
-            navbarCollapsible.classList.add("navbar-shrink");
-        }
-    };
-
-    // Shrink the navbar
-    navbarShrink();
-
-    // Shrink the navbar when page is scrolled
-    document.addEventListener("scroll", navbarShrink);
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector("#mainNav");
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: "#mainNav",
-            offset: 72,
-        });
+window.addEventListener('DOMContentLoaded', (event) => {
+  // Navbar shrink function
+  const navbarShrink = function () {
+    const navbarCollapsible = document.body.querySelector('#mainNav')
+    if (!navbarCollapsible) {
+      return
     }
+    if (window.scrollY === 0) {
+      navbarCollapsible.classList.remove('navbar-shrink')
+    } else {
+      navbarCollapsible.classList.add('navbar-shrink')
+    }
+  }
 
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector(".navbar-toggler");
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll("#navbarResponsive .nav-link")
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener("click", () => {
-            if (window.getComputedStyle(navbarToggler).display !== "none") {
-                navbarToggler.click();
-            }
-        });
-    });
-    const swiper = new Swiper('.blog-slider', {
-        spaceBetween: 30,
-        effect: 'fade',
-        loop: true,
-        mousewheel: {
-            invert: false,
-        },
-        // autoHeight: true,
-        pagination: {
-            el: '.blog-slider__pagination',
-            clickable: true,
-        }
-    });
+  // Shrink the navbar
+  navbarShrink()
 
-    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-        return new bootstrap.Popover(popoverTriggerEl)
+  // Shrink the navbar when page is scrolled
+  document.addEventListener('scroll', navbarShrink)
+
+  // Activate Bootstrap scrollspy on the main nav element
+  const mainNav = document.body.querySelector('#mainNav')
+  if (mainNav) {
+    new bootstrap.ScrollSpy(document.body, {
+      target: '#mainNav',
+      offset: 72
     })
-    $(document).ready(function () {
-        $('#example').DataTable();
-    });
-    $('.toast').toast('show')
-});
+  }
+
+  // Collapse responsive navbar when toggler is visible
+  const navbarToggler = document.body.querySelector('.navbar-toggler')
+  const responsiveNavItems = [].slice.call(
+    document.querySelectorAll('#navbarResponsive .nav-link')
+  )
+  responsiveNavItems.map(function (responsiveNavItem) {
+    responsiveNavItem.addEventListener('click', () => {
+      if (window.getComputedStyle(navbarToggler).display !== 'none') {
+        navbarToggler.click()
+      }
+    })
+  })
+  const swiper = new Swiper('.blog-slider', {
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: true,
+    mousewheel: {
+      invert: false
+    },
+    // autoHeight: true,
+    pagination: {
+      el: '.blog-slider__pagination',
+      clickable: true
+    }
+  })
+
+  const popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  const popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+  })
+  $(document).ready(function () {
+    $('#example').DataTable()
+  })
+  $('.toast').toast('show')
+})
